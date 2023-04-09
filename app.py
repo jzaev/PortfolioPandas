7ad59574010dfe3ce0e8f65d6779b4ef6e86f6e9
Yuri Zaev <juryzaev@gmail.com>
2023-04-09 12:58:15 +0700

    Changed csv to Nobel

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-
 @app.route('/', methods=['GET'])
 def upload_file():
-    filepath = "static/organizations-10000.csv"
+    filepath = "static/complete.csv"
     data = {}
     if filepath:
         df = pd.read_csv(filepath)
@@ -22,71 +21,49 @@ def upload_file():
         # 2
         data["shape"] = (df.shape, '>> shape = df.shape <br>>>  f"Rows: {shape[0]}, Columns: {shape[1]}"')
         # 3
-        data["columns"] = (pd.DataFrame(df.dtypes).reset_index().to_html(classes='res-table'),
+        data["columns"] = (pd.DataFrame(df.dtypes).reset_index().to_html(classes='res-table toggle-table'),
                            ">> pd.DataFrame(df.dtypes).reset_index()")
         # 4
         data["describe"] = (df.describe().to_html(classes='res-table'), ">> df.describe()")
         # 5
         data["nan_count"] = (
-        pd.DataFrame(df.isna().sum(), columns=["NaN count"]).reset_index().to_html(classes='res-table'),
+        pd.DataFrame(df.isna().sum(), columns=["NaN count"]).reset_index().to_html(classes='res-table toggle-table'),
         '>> pd.DataFrame(df.isna().sum(), columns=["NaN count"]).reset_index()')
         # 6
         data["empty_count"] = (
-            pd.DataFrame(df.isin(["", " "]).sum(), columns=["Empty count"]).reset_index().to_html(classes='res-table'),
+            pd.DataFrame(df.isin(["", " "]).sum(), columns=["Empty count"]).reset_index().to_html(classes='res-table toggle-table'),
             '>> pd.DataFrame(df.isin(["", " "]).sum(), columns=["Empty count"]).reset_index()')
         # 7
         data["duplicates_count"] = (f"{df.duplicated().sum()} (rows)", 'f"{df.duplicated().sum()} (rows)"')
-        # 8
-        country_column = "Country"
-        data["unique_countries"] = (
-        df[country_column].value_counts().reset_index().to_html(classes='res-table', table_id='table8'),
-        f">> df['{country_column}'].value_counts().reset_index()")
+        # Здесь будут новые блоки кода для анализа данных из нового файла CSV
+        #8
+        columns_to_drop = [
+            "prizeAmount", "categoryFullName", "sortOrder", "portion", "prizeAmount",
+            "dateAwarded", "motivation", "categoryTopMotivation", "award_link", "givenName",
+            "familyName", "fullName", "penName", "birth_city", "birth_country", "birth_locationString",
+            "death_date", "death_city", "death_cityNow", "death_continent", "death_country",
+            "death_countryNow", "death_locationString", "nativeName", "acronym", "org_founded_date",
+            "org_founded_city", "org_founded_cityNow", "org_founded_continent", "org_founded_country",
+            "org_founded_countryNow", "org_founded_locationString", "residence_1", "residence_2",
+            "affiliation_1", "affiliation_2", "affiliation_3", "affiliation_4","laureate_link","orgName"
+        ]
+
+        df = df.drop(columns=columns_to_drop)
+
+        # 8. Data after dropping unnecessary columns
+        data["dropped_columns"] = (df.sample(5).to_html(classes='res-table', index=False),
+                                   ">> columns_to_drop = [...] <br>>> df = df.drop(columns=columns_to_drop) <br>>> df.sample(5)")
         # 9
-        data["grouped_by_country"] = (
-        df.groupby("Country")["Number of employees"].sum().sort_values(ascending=False).reset_index().to_html(
-            classes='res-table', table_id='table9'),
-        f">> df.groupby('Country')['employees_column'].sum().sort_values(ascending=False).reset_index()")
+        data["nan_count2"] = (
+        pd.DataFrame(df.isna().sum(), columns=["NaN count"]).reset_index().to_html(classes='res-table'),
+        '>> pd.DataFrame(df.isna().sum(), columns=["NaN count"]).reset_index()')
+
         # 10
-        data["unique_industries"] = (
-        df["Industry"].value_counts().reset_index().to_html(classes='res-table', table_id='table10'),
-        f">> df['Industry'].value_counts().reset_index()")
-
-        # ===========GRAPH===================
-
-        fig1 = px.bar(df["Country"].value_counts().head(10),
-                      x=df["Country"].value_counts().head(10).index,
-                      y=df["Country"].value_counts().head(10).values,
-                      labels={"x": "Country", "y": "Number of organizations"},
-                      title="Top 10 countries by number of organizations")
-        fig1.write_image("static/images/top_countries_plotly.png")
-
-        top_industries = df["Industry"].value_counts().head(10)
-        fig2 = px.bar(top_industries,
-                      x=top_industries.values,
-                      y=top_industries.index,
-                      orientation='h',
-                      labels={"x": "Number of organizations", "y": "Industry"},
-                      title="Top 10 industries by number of organizations")
-        fig2.write_image("static/images/top_industries_plotly.png")
-
-        industry_counts = df['Industry'].value_counts().nlargest(15)
-
-        fig3 = go.Figure(go.Bar(
-            x=industry_counts.values,
-            y=industry_counts.index,
-            orientation='h'
-        ))
-
-        fig3.update_layout(
-            title="Top 15 Industries",
-            xaxis_title="Number of Companies",
-            yaxis_title="Industry",
-        )
-
-        fig3.write_image("static/images/top15_industries_plotly.png")
+        data["rows_with_nan"] = (
+            df[df.isna().any(axis=1)].reset_index().to_html(classes='res-table toggle-table', index=False),
+            '>> df[df.isna().any(axis=1)]')
 
     return render_template('upload.html', data=data)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
